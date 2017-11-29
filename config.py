30b1ba63a33d86cea84ee30a4ea70b8241810417
Cory Gray <cgray@localhost.localdomain>
2017-11-29 13:05:35 -0500

    Committing so I can pull

--- config.py
+++ config.py
@@ -1,3 +1,13 @@
+import os
+import pymysql
+from flask_sqlalchemy import SQLAlchemy
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = "mysql+pymysql://crgray21:Eaglesfan21!@mysql-prison-db.cprezpfpg9xo.us-east-1.rds.amazonaws.com/prison_rdb"
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
 WTF_CSRF_ENABLED = True #activates cross-site forgery prevention
 SECRET_KEY = '1234'
 
@@ -7,3 +17,4 @@ OPENID_PROVIDERS = [
     {'name': 'AOL', 'url': 'http://openid.aol.com/<username>'},
     {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
     {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}]
+
